50cf681306353b3b2047d4aaa04fa66062b02af7
MZB-DS <49339352+MZB-DS@users.noreply.github.com>
2020-07-29 10:56:22 +0530

    Add files via upload

---
+++ slack_channel_alert.py
@@ -0,0 +1,23 @@
+from slackclient import SlackClient
+import time
+from sqlquery_to_df import get_data
+
+token = '' # Enter your slack app token
+slack_client = SlackClient(token)
+
+count=0
+
+while True:
+
+  queries = 'select count(*) from people_information where city="PUNE";'
+  column_names = ["Count"]
+  sql_query_output_df = get_data(queries, column_names)
+
+  count1 = sql_query_output_df["Count"][0]
+
+  if count1>count:
+    count=count1
+    text="New person added in the Pune city. Kindly check" #Message you want to post.
+    slack_client.api_call("chat.postMessage",channel='Channel_Name',text=text) # Enter the slack channel yo want to trigger this alert on.
+
+  time.sleep(30)
